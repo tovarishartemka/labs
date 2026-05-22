7cd5f08477780c520dddaf6ae838722ba55e3a73
tovarishartemka <93365392+tovarishartemka@users.noreply.github.com>
2026-05-23 00:46:50 +0300

    Update calc.cpp

--- lab2/calc.cpp
+++ lab2/calc.cpp
@@ -1,34 +1,43 @@
 #include <iostream>
-#include <chrono>
+#include <ctime>
 
-double calculate (double x) {
-    return x*x - x*x + x*4 - x*5 + x + x;
+double calculate(double x)
+{
+    return x * x - x * x + x * 4 - x * 5 + x + x;
 }
 
-
-
 int main()
-
 {
     long long n;
-    double res, time;
-    int x = 10;
+    double res = 0;
+    double x = 10.0;
 
-    while(1)
+    while (true)
     {
-        if (!(std::cin >> n)) 
-            break; 
-        
+        std::cout << "Input number of iterations: ";
+
+        if (!(std::cin >> n))
+        {
+            std::cout << "Input is not a number. Program finished.\n";
+            break;
+        }
+
         clock_t start = clock();
 
-        for (long long i =0; i<n; ++i)
+        for (long long i = 0; i < n; ++i)
         {
             res = calculate(x);
         }
 
         clock_t end = clock();
-        time = (double)(end - start) / CLOCKS_PER_SEC;
 
-        std::cout <<"time =  "<< time <<";  "<<"result(x=10) =  "<< res << "\n";
+        double elapsed_time =
+            static_cast<double>(end - start) / CLOCKS_PER_SEC;
+
+        std::cout << "Time = " << elapsed_time
+            << " sec; Result(x = 10) = "
+            << res << "\n\n";
     }
+
+    return 0;
 }
